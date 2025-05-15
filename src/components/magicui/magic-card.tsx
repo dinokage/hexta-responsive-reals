f290eaf2509bbbc3624fedf570c21d5fdafdd1f9
Dinesh Yerra <68950956+dinokage@users.noreply.github.com>
2025-05-15 14:53:22 +0530

    v3 card for magicui

--- src/components/magicui/magic-card.tsx
+++ src/components/magicui/magic-card.tsx
@@ -87,7 +87,7 @@ export function MagicCard({
           radial-gradient(${gradientSize}px circle at ${mouseX}px ${mouseY}px,
           ${gradientFrom}, 
           ${gradientTo}, 
-          var(--border) 100%
+          hsl(var(--border)) 100%
           )
           `,
         }}
